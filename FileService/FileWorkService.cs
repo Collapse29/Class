0343a38f32d8d0de8d1658b8c368828afe80c3c3
Collapse29 <seryyzotov@gmail.com>
2023-04-14 21:39:27 +0500

    СОздали текстовую базу данных машин, создали сервис для чтения файлов

---
+++ FileService/FileWorkService.cs
@@ -0,0 +1,12 @@
+﻿namespace FileService
+{
+    public class FileWorkService
+    {
+        public string[] ReadFile(string path)
+        {
+            string[] data = File.ReadAllLines(path);
+
+            return data;
+        }
+    }
+}
--- InHerit/Program.cs
+++ InHerit/Program.cs
@@ -1,4 +1,5 @@
-﻿using InHerit.Auto;
+﻿using FileService;
+using InHerit.Auto;
 
 namespace InHerit
 {
@@ -6,35 +7,54 @@ namespace InHerit
     {
         static void Main(string[] args)
         {
-            TS ts = new TS();//Создали переменную класса ts
+            //TS ts = new TS();//Создали переменную класса ts
 
-            ts.WriteData();
+            //ts.WriteData();
 
-            ts = new TS(5, 7); //Создаю новый объект класса
+            //ts = new TS(5, 7); //Создаю новый объект класса
 
-            ts.WriteData();
+            //ts.WriteData();
 
-            ts.ChangeData(8, 10);// Меняю значения
+            //ts.ChangeData(8, 10);// Меняю значения
 
-            ts.WriteData();
+            //ts.WriteData();
 
-            AutoBase afto = new AutoBase(5, 10, "Двигатель 1", "Nissan");
+            //AutoBase afto = new AutoBase(5, 10, "Двигатель 1", "Nissan");
 
-            afto.WriteData();
+            //afto.WriteData();
 
-            afto.ChangeData(4, 8, "Двигатель 2", "Nissan");
-            afto.WriteData();
-            Console.WriteLine("*******");
+            //afto.ChangeData(4, 8, "Двигатель 2", "Nissan");
+            //afto.WriteData();
+            //Console.WriteLine("*******");
             AutoPark autoPark = new AutoPark();
 
-            autoPark.AddCar(null);
-            autoPark.AddCar(new AutoBase(new Random().Next(0, 10), new Random().Next(0, 20),$"Двигатель  {new Random().Next(0, 100)}", "Kia"));
-            autoPark.AddCar(new AutoBase(new Random().Next(0, 10), new Random().Next(0, 20), "Двигатель", "Kia"));
-            autoPark.AddCar(new AutoBase(new Random().Next(0, 10), new Random().Next(0, 20), "Двигатель", "Kia"));
+            //autoPark.AddCar(null);
+            //autoPark.AddCar(new AutoBase(new Random().Next(0, 10), new Random().Next(0, 20), $"Двигатель  {new Random().Next(0, 100)}", "Kia"));
+            //autoPark.AddCar(new AutoBase(new Random().Next(0, 10), new Random().Next(0, 20), "Двигатель", "Kia"));
+            //autoPark.AddCar(new AutoBase(new Random().Next(0, 10), new Random().Next(0, 20), "Двигатель", "Kia"));
+            //autoPark.WriteCars();
+            //Console.WriteLine("*******");
+            //autoPark.DeletCar(3);
+            //autoPark.WriteCars();
+
+            FileWorkService fileWorkService = new FileWorkService();
+
+            string[] autoData = fileWorkService.ReadFile(@"D:\Учеба\C#\Class\InHerit\dataBase.txt");
+            for (int currentRow = 0; autoData.Length > currentRow; currentRow++)
+            {
+                string line = autoData[currentRow];
+                string[] element = line.Split('/');
+                AutoBase newCar = new AutoBase(int.Parse(element[0]), int.Parse(element[1]), element[2], element[3]);
+                autoPark.AddCar(newCar);
+            }
             autoPark.WriteCars();
-            Console.WriteLine("*******");
-            autoPark.DeletCar(3);
-            autoPark.WriteCars();
+
+            foreach (string currentRow in autoData)
+            {
+                string[] element = currentRow.Split('/');
+                AutoBase newCar = new AutoBase(int.Parse(element[0]), int.Parse(element[1]), element[2], element[3]);
+                autoPark.AddCar(newCar);
+            }
         }
 
     }
